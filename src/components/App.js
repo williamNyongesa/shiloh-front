285650dd7755cf4b0a00cf481b720e81a44fe19e
Medrine Mulindi <medrine.mulindi@gmail.com>
2024-10-21 20:33:05 +0300

    creates components directory

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+// import logo from './logo.svg';
+// import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1 className="text-center text-2xl p-4 text-blue-500 font-bold">Welcome to Shiloh School Management System</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
