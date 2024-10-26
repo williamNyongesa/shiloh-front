5f8435f53fd69d12177ab35b4e88ceae883cc76e
Medrine Mulindi <medrine.mulindi@gmail.com>
2024-10-26 15:57:11 +0300

    cinfigures project colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,8 +4,15 @@ module.exports = {
     './src/**/*.{js,jsx,ts,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#D9D9D9',  
+        secondary: '#D1D1D1',
+        accent: '#FF6347',   
+        background: '#F9F9F9',
+        text: '#333333',     
+      },
+    },
   },
   plugins: [],
 }
-
